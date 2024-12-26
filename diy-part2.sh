7de515ac1ace9281bfdf3413f9829da2c3fcadf3
PanicMaker <mxc2012@outlook.com>
2024-12-26 17:10:21 +0800

    Update

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,43 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Wan口LAN口互换
+sed -i 's/ucidef_set_interface_lan '\''eth0''/ucidef_set_interface_lan '\''eth1''/g' package/base-files/files/etc/board.d/99-default_network
+sed -i 's/ucidef_set_interface_wan '\''eth1''/ucidef_set_interface_wan '\''eth0''/g' package/base-files/files/etc/board.d/99-default_network
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 移除冲突文件
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing-box*,smartdns,brook*,chinadns-ng,*dns2socks*,dns2tcp*shadowsocks-libev,*shadowsocks-rust,*simple-obfs,*tcping,*trojan-go.*trojan,*trojan-plus,*tuic-client,*hysteria}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# 在线用户
+git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
+sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
+chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+
+# 加入第三方源
+git clone --depth=1 https://github.com/kenzok8/small package/small
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/kenzo
+
+# 更换golong
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+./scripts/feeds update -a
+./scripts/feeds install -a
